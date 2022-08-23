31a1d110ebbd9748d6b0e075a73a88906f18c032
realclownblack <danilowilliam3255@gmail.com>
2022-08-23 16:56:33 -0300

    23/08/2022 - 16:56

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,32 +5,88 @@ import facebook from '../../assets/img/facebook.png'
 import instragram from '../../assets/img/instagram.png'
 import whatsapp from '../../assets/img/whatsapp.png'
 import youtube from '../../assets/img/youtube.png'
+import logo from '../../assets/img/earth bank.png'
 export const Footer = () => {
 
        return (
-              <div className="Footer">
-                     <div className="footer_1">
+              <footer className="Footer">
+                      <div className="footer_1">
+                            <img className='logo' src={logo} alt="" />
+                            <div className='Baixaapp'>
+                                   <p>Baixa App</p>
+
+                                   <img src={app_store} alt="" />
+
+                                   <img src={google_play} alt="" />
+
+                            </div>
+                            <div className='Atendimento'>
+                                   <p>Atendimento</p>
+
+                                   <p>Central de Relacionamento <br />
+                                   0800 275 6398</p>
+
+                                   <p>SAC <br />
+                                   0800 722 6398</p>
+
+                                   <p>Ouvidoria <br />
+                                   0800 688 6398</p>
+
+                                   <p>falaEarth@gmail.com</p>
+
+                                   <i>Outros Contatos</i>
+
+                            </div>
+                            <div Informações>
+                                   <p>Informações</p>
+
+                                   <p>Caixas Eletrônicos</p>
+
+                                   <p>LGPD</p>
+
+                                   <p>Diretiva de Privacidade</p>
+
+                                   <p>Documentos Importantes</p>
+
+                                   <p>Cotação do Dólar da sua fatura</p>
+
+                                   <p>Procon</p>
+
+                                   <p>Código de Defesa do Consumidor</p>
+                            </div>
+                            <div className='Empresa'>
+                                   <p>Empresa</p>
+
+                                   <p>What’s earth</p>
+
+                                   <p>Sobre o earth</p>
+
+                                   <p>Carreiras</p>
+                            </div>
+                     </div> 
+                     <div className="footer_2">
                             <div className='Text_Footer'>
-                                   <p>Next Tecnologia e Serviços Digitais © 2020. <br />
+                                   <p className='text_nu1'>Earth Tecnologia e Serviços Digitais © 2020. <br />
                                    Todos os direitos reservados. CNPJ: nº 15.011.336/0001-27 <br />
                                    Rua Domingos Sérgio dos Anjos, 277 - Jardim Santo Elias, São Paulo - SP, 05136-170</p>
 
-                                   <p>
+                                   <p className='text_nu2'>
                                    O Next Tecnologia e Serviços Digitais, Brasil, <br />
                                    é uma plataforma digital que atua como correspondente no País de uma instituição financeira, <br />
                                    nos termos da Resolução CMN n° 3.954 de 24 de fevereiro de 2011.
                                    </p>
 
-                                   <p>
+                                   <p className='text_nu3'>
                                    O next não é uma instituição financeira, <br />
                                    de modo que quem realiza as operações de crédito, <br />
-                                   manutenção de contas e demais atividades financeiras é o BANCO BRADESCO S.A., banco múltiplo, inscrito no CNPJ sob nº 60.746.948.0001-12, <br />
+                                   manutenção de contas e demais atividades financeiras é o BANCO BRADESCO S.A., <br />
+                                    banco múltiplo, inscrito no CNPJ sob nº 60.746.948.0001-12, <br />
                                    com sede na Cidade de Deus, s/n, Vila Yara, Osasco, SP, CEP 06029-900.
                                    </p>
                             </div>
-                            <div className='Redes_fopter'>
-                                   <p>SIGA EARTH</p>
+                            <div className='Redes_footer'>
                                    <div className='Logo_redes'>
+                                          <p>SIGA EARTH</p>
                                           <img className='redes_logo' src={facebook} alt="facebook" />
                                           <img className='redes_logo' src={instragram} alt="instragram" />
                                           <img className='redes_logo' src={youtube} alt="youtube" />
@@ -39,9 +95,6 @@ export const Footer = () => {
 
                             </div>
                      </div>
-                     <div className="footer_2">
-                            <p>jhfaswyuas\zfoyasff</p>
-                     </div>
-              </div>
+              </footer>
        )
 }
