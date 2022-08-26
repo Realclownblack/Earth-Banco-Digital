43b2f79862ff23ac1ab25cd9298e1e4e543dd8e9
realclownblack <danilowilliam3255@gmail.com>
2022-08-26 09:07:44 -0300

    26/08/2022 -09:07

--- src/components/vantagens/Vantagens.jsx
+++ src/components/vantagens/Vantagens.jsx
@@ -2,7 +2,17 @@ import '../../assets/css/vantagens.css'
 export const Vantagens = () => {
     return (
         <div className="container">
-            
+            <div>
+                <div className='amigos'>
+
+                </div>
+                <div className='tarifa'>
+
+                </div>
+                <div className='cdb'>
+
+                </div>
+            </div>
         </div>
     )
 }
