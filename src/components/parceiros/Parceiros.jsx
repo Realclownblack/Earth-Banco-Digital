954ba0099b201ee8309e0eedb781a2866f207662
realclownblack <danilowilliam3255@gmail.com>
2022-08-28 16:46:44 -0300

    28/08/2022 - 16:46

--- src/components/parceiros/Parceiros.jsx
+++ src/components/parceiros/Parceiros.jsx
@@ -1,9 +1,23 @@
 import '../../assets/css/parceiros.css'
-
+import coca from '../../assets/img/coca.png'
 export const Parceiros = () => {
     return (
         <div className="parceiros">
-
+            <div className='info_parceiros'>
+                <div className='primeiro_1'>
+                    <img className='coca' src={coca} alt="" />
+                </div>
+                <div className='primeiro_2'>
+                    <img className='coca' src={coca} alt="" />
+                </div>
+                <div className='primeiro_3'>
+                    <img className='coca' src={coca} alt="" />
+                </div>
+                
+            </div>
+            <div className='info_logo'>
+                <img className='coca' src={coca} alt="" />
+            </div>
         </div>
     )
 }
