0fd5f48d202e17d4448eca8d623b1d58e1d441ae
realclownblack <danilowilliam3255@gmail.com>
2022-09-15 11:35:17 -0300

    15/09/2022 - 11:35

--- src/components/menu/CustomMenu.jsx
+++ src/components/menu/CustomMenu.jsx
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import { fallDown as Menu } from 'react-burger-menu'
 
-import google_play from '../../assets/img/google-play.svg'
-import app_store from '../../assets/img/app-store.svg'
-import facebook from '../../assets/img/facebook.png'
-import instragram from '../../assets/img/instagram.png'
-import whatsapp from '../../assets/img/whatsapp.png'
-import youtube from '../../assets/img/youtube.png'
 
 class CustomMenu extends Component {
     state = {
@@ -32,67 +26,142 @@ class CustomMenu extends Component {
         return (
             <Menu  className={scroll ? "customMenu Aparecer_burger-button" : "customMenu"} onOpen={ abriuMenu } isOpen={ this.state.menuAberto }  onClose={fechouMenu}    >
             <div className='Opcoes'>
-                <div className='colun1'>
-                    <div className='conta'>
-                        <p>Account</p>
-                        <a id="Account" className="menu-item" href="/">Account</a>
+                    <div className='colun0'>
+                        <div className='informacoes'>
+                            <p className='text_titulo_conta'>Conta</p>
+                            <span>
+                                <p className='text_titulo_menu'>Conta Earth</p>
+                                <p className='text_span_menu'>Conta grátis com serviços ilimitados.</p>
+                            </span>
+                            <p className='text_titulo_menu'>EarthJoy</p>
+                            <p className='text_span_menu'>Conta digital e gratuita para menores de idade, com toda magia da Disney.</p>
+                        </div>
+                        <hr />
+                        <p className='text_tem'>Tem no Earth</p>
+                    </div>                
+                <div className='colunas'>
+                    
+                    <div className='colun1'>
+                        <ul>
+                            <li>
+                                <div className='caixa_menu'>
+                                    <span>
+                                        <p>Account</p>
+                                        <p>Cartões sem anuidade, pagamentos digitais e mimos exclusivos.</p>
+                                        <a id="Account" className="menu-item" href="/">Account</a>
+                                    </span>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='caixa_menu'>
+                                    <span>
+                                        <p>Have on Earth</p>
+                                        <a id="Have_on_Earth" className="menu-item" href="/about">Have on Earth</a>                                        
+                                    </span>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='caixa_menu'>
+                                    <span>
+                                        <p>Loans</p>
+                                        <a id="Loans" className="menu-item" href="/contact">Loans</a>                                        
+                                    </span>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='caixa_menu'>
+                                    <span>
+                                        <p>Loans</p>
+                                        <a id="Loans" className="menu-item" href="/contact">Loans</a>                                        
+                                    </span>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='caixa_menu'>
+                                    <span>
+                                        <p>Loans</p>
+                                        <a id="Loans" className="menu-item" href="/contact">Loans</a>                                        
+                                    </span>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='caixa_menu'>
+                                    <span>
+                                        <p>Loans</p>
+                                        <a id="Loans" className="menu-item" href="/contact">Loans</a>                                        
+                                    </span>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='caixa_menu'>
+                                    <span>
+                                        <p>Loans</p>
+                                        <a id="Loans" className="menu-item" href="/contact">Loans</a>                                        
+                                    </span>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='caixa_menu'>
+                                    <span>
+                                        <p>Loans</p>
+                                        <a id="Loans" className="menu-item" href="/contact">Loans</a>                                        
+                                    </span>
+                                </div>
+                            </li>
+                        </ul>
+
                     </div>
-                    <div className='temearth'>
-                        <p>Have on Earth</p>
-                        <a id="Have_on_Earth" className="menu-item" href="/about">Have on Earth</a>
-                    </div>
-                    <div className='emprestimos'>
-                        <p>Loans</p>
-                        <a id="Loans" className="menu-item" href="/contact">Loans</a>
-                    </div>
+                <div className='colun2'>
+                        <ul>
+                            <li>
+                                <div className='caixa_menu'>
+                                    <span>
+                                        <p>Open Finance</p>
+                                        <a id="Open_Finance" className="menu-item" href="/contact">Open Finance</a>                                        
+                                    </span>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='caixa_menu'>
+                                    <span>
+                                        <p>Help</p>
+                                        <a id="Help" className="menu-item" href="/contact">Help</a>                                        
+                                    </span>
+                                </div>                        
+                            </li> 
+                            <li>
+                                <div className='caixa_menu'>
+                                    <span>
+                                        <p>About Us</p>
+                                        <p>O banco 100% digital que veio pra ajudar a fazer os planos acontecerem.</p>
+                                        <a id="About Us" className="menu-item" href="/contact">About Us</a>                                        
+                                    </span>
+                                </div>                        
+                            </li>
+                            <li>
+                                <div className='caixa_menu'>
+                                    <span>
+                                        <p>Loans</p>
+                                        <a id="Loans" className="menu-item" href="/contact">Loans</a>                                        
+                                    </span>
+                                </div>
+                            </li>
+                            <li>
+                                <div className='caixa_menu'>
+                                    <span>
+                                        <p>Loans</p>
+                                        <a id="Loans" className="menu-item" href="/contact">Loans</a>                                        
+                                    </span>
+                                </div>
+                            </li>  
+                        </ul>
+                </div>                    
                 </div>
-               <div className='colun2'>
-                    <div className='open'>
-                        <p>Open Finance</p>
-                        <a id="Open_Finance" className="menu-item" href="/contact">Open Finance</a>
-                    </div>
-                    <div className='ajuda'>
-                        <p>Help</p>
-                        <a id="Help" className="menu-item" href="/contact">Help</a>
-                    </div>
-                    <div className='sobre'>
-                        <p>About Us</p>
-                        <a id="About Us" className="menu-item" href="/contact">About Us</a>
-                    </div>
-               </div>
+
                 
 
             </div>
            
-            <div className='Barra_direita'>
-                <div className='Barra_dentro'>
-                    <p>Ajuda</p>
-                    <a className='button' href="Dúvidas">Dúvida</a>
-                    <br />
-                    <a className='button' href="Contato">Contato</a>
-                <p>Central de relacionamento <br />
-                    0800 275 6398</p>
-                <p>SAC <br />
-                    0800 722 6398</p>
-                <p>falaearth@gmail.com</p>
-                <p className='Siga'>Siga Earth</p>
-                <div className='redes'>
-                    <img className='redes_logo' src={facebook} alt="facebook" />
-                    <img className='redes_logo' src={instragram} alt="instragram" />
-                    <img className='redes_logo' src={youtube} alt="youtube" />
-                    <img className='redes_logo' src={whatsapp} alt="whatsapp" />
-                </div>
-
-                </div>
-                
-            </div>
-            <div className='Barra_direta1'>
-                <p className='Text_store'>BAIXE O SEU APLICATIVO E ABRA SUA CONTA:</p>
-                <div className='store_logo'>
-                <img className='Logo_store' src={google_play} alt="Connect-Banco-Digital logo"></img>
-                <img className='Logo_store' src={app_store} alt="Connect-Banco-Digital logo"></img>
-                </div>
-            </div>
           </Menu>
         );
     }
