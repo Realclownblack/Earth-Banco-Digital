203f5208d2c7ddeb2760e7cd2a39fe1e5740bc70
realclownblack <danilowilliam3255@gmail.com>
2022-09-15 09:24:25 -0300

    15/09/2022 - 09:24

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import '../../assets/css/navbar.css'
-import img from '../../assets/img/retrato-de-uma-jovem-surpreendida.png'
+import img from '../../assets/img/img_8.png'
 export const Navbar = () => {
     return(
         <div>
@@ -8,16 +8,19 @@ export const Navbar = () => {
                     <div className='img-div'>
                         <img className='img-mulhe' src={img} alt="" />
                     </div>
-                    <p>Conta grátis, <br /> 
-                    100% digital e com serviços ilimitados</p>
-                    <br />
-                    <span>
-                        <p>para você</p>
-                    </span>
-                    <br />
-                    <span>
-                        <p>fazer acontecer</p>
-                    </span>
+                    <div className='text_nav'>
+                        <p>Conta grátis, <br /> 
+                        100% digital e com serviços ilimitados</p>
+                        <br />
+                        <span>
+                            <p>para você</p>
+                        </span>
+                        <br />
+                        <span>
+                            <p>fazer acontecer</p>
+                        </span> 
+                    </div>
+
                 </div>
                 
             </div>
